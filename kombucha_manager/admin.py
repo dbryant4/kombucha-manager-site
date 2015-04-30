933a67571e096d712e4b6056c0b081294125e0fb
Derrick Bryant <dbryant4@gmail.com>
2015-04-30 17:32:09 -0400

    Added user profile to inline

--- kombucha_manager/admin.py
+++ kombucha_manager/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
-
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 from .models import *
+
+class UserProfileInline(admin.StackedInline):
+	model = UserProfile
+
+class UserAdmin(UserAdmin):
+	inlines = (UserProfileInline,)
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 
 admin.site.register(Source)
 admin.site.register(TeaType)
