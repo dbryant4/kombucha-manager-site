7d4d922b0dd4dc737b90c47cb897f6efb520c448
Derrick Bryant <dbryant4@gmail.com>
2015-11-15 20:29:31 -0800

    Switch port to 5000

--- provision/files/docker-entrypoints.sh
+++ provision/files/docker-entrypoints.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+python manage.py bower install --no-color
+python manage.py compress --force
+python manage.py collectstatic --no-color --noinput
 python manage.py migrate
-python manage.py loaddata kombucha_manager dev --no-color
-python manage.py runserver 0.0.0.0:8000
+python manage.py loaddata kombucha_manager/fixtures/dev.json --no-color
+python manage.py test
+pip install honcho
+honcho start
