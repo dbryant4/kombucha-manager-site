a05a75c6baa9c4c05fee76b6c1d8875296a14dd1
Derrick Bryant <dbryant4@gmail.com>
2015-11-22 21:16:44 -0800

    Add docker testing framework

---
+++ run_docker_tests.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker-compose --file docker-compose-test.yml up --force-recreate -d db
+docker-compose --file docker-compose-test.yml up --force-recreate web
+docker-compose --file docker-compose-test.yml stop db
+docker-compose --file docker-compose-test.yml rm --force -v
